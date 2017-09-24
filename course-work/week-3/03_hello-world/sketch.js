f204d53677c7514ca111b066ac56a1ba5c2ddb9f
Michael Musick <michael@michaelmusick.com>
2017-09-23 19:56:59 -0600

    work from videos

---
+++ course-work/week-3/03_hello-world/sketch.js
@@ -0,0 +1,29 @@
+/*
+
+Everything inside of the block comment is igniored
+
+hahaha
+cylinder(shloj,height,detail)
+
+ */
+
+
+
+
+// This is an in-line comment
+// This is another comment.
+
+// This is the setup() function definition
+// This is required by p5 for setup
+function setup(){
+}
+
+// This is the draw() function definition
+// This gets called after setup() finishes executing
+function draw(){
+
+    // This will draw a circle
+    // centered at point (20, 20)
+    // This circle will have a diameter of 30 pixels
+    ellipse( 20, 20, 30, 30 );  // everything after these slashes, is ignored
+}
