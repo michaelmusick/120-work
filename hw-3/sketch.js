02b747d7d0c21c4348867cfc671300cda271ce09
Michael Musick <michael@michaelmusick.com>
2017-09-24 15:52:14 -0600

    adding course work p5 example. And, commiting hw-3

---
+++ hw-3/sketch.js
@@ -0,0 +1,17 @@
+// Declare the setup function
+function setup() {
+
+    // create a canvas 600px wide and 400px high
+    createCanvas( 600, 400 );
+
+    // color the background 'pink'
+    background( 'pink' );
+}
+
+
+// Declare draw() function
+function draw(){
+
+    // using the text function, have p5 write, "Hello World!"
+    text( "Hello World!", 300, 200 );
+}
